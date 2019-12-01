978b170255b89a406f80b85583a884b04cab3a53
Sebastien Ethier <sebastien.ethier@seconceptweb.ca>
2019-12-01 17:08:16 -0500

    Using factory to inject dependency for database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import {AppService} from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import {QuotesModule} from './quotes/quotes.module';
 import {LoggerMiddleware} from './shared/middleware/logger.middleware';
+import {ConfigModule} from './config/config.module';
+import {ConfigService} from './config/config.service';
 
 @Module({
-    imports: [MongooseModule.forRoot(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    }), QuotesModule],
+
+    imports: [MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+            uri: configService.get('MONGO_URI'),
+        }),
+        inject: [ConfigService],
+    }), QuotesModule, ConfigModule],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/config/config.module.ts
@@ -0,0 +1,15 @@
+import {Module} from '@nestjs/common';
+import {ConfigService} from './config.service';
+
+@Module({
+    providers: [
+        {
+            provide: ConfigService,
+            // useValue: new ConfigService(`${process.env.NODE_ENV || 'development'}.env`),
+            useValue: new ConfigService((process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod') ? '.env.prod' : '.env'),
+        },
+    ],
+    exports: [ConfigService],
+})
+export class ConfigModule {
+}
