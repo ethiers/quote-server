5d311cd46c23e37f8d49fe19b5f7bd1e9b105db2
Sebastien Ethier <sebastien.ethier@seconceptweb.ca>
2019-12-01 21:02:11 -0500

    Add my name and my certificate link

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Baljeet Singh !';
+    return 'Sebastien Ethier, <a href="https://www.udemy.com/certificate/UC-YW5L2W0E/">Udemy Certificate</a>, Instructor: Baljeet Singh';
   }
 }
